7a40268d346c5b776f04352946b2d47fb336f7c8
Ben May <benjgmay@googlemail.com>
2018-10-15 21:12:57 +0100

    Exercises 32, 34 and  34

---
+++ ex34.py
@@ -0,0 +1,17 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("I think the animal at 1 would be python 3.6. It is", animals[1])
+
+print("I think the third (3rd) animal would be peacock. It is", animals[2])
+
+print("I think the first (1st) animal would be bear. It is", animals[0])
+
+print("I think the animal at 3 would be kangaroo. It is", animals[3])
+
+print("I think the fifth (5th) animal would be whale. It is", animals[4])
+
+print("I think the animal at 2 would be peacock. It is", animals[2])
+
+print("I think the sixth (6th) animal would be platypus. It is", animals [5])
+
+print("I think the animal at 4 would be whale. It is", animals[4])
