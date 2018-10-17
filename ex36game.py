908ca3ec67e578629004c008e2e12d202ef3ad5c
Ben May <benjgmay@googlemail.com>
2018-10-17 22:24:51 +0100

    Updated game with first steps of road to kee

--- ex36game.py
+++ ex36game.py
@@ -1,5 +1,7 @@
 ## Based on Sailors on the Starless Sea
+player_inventory = []
 
+player_hp = 10
 
 def stairs_to_darkness():
     print("Stairs to darkness and end")
@@ -35,6 +37,37 @@ def gatehouse():
 def road_to_keep():
     print(">>>> Road to keep")
 
+    print('\n As you walk down the road you take stock of your situation.')
+    print('You are the only one in your village brave enough to strike against',
+    'the evil that steals your kinfolk in the night.')
+    print('You alone hold the blade known to the world as...\n')
+
+    sword = input('> ')
+    player_inventory.append(sword)
+
+    print("\nIndeed, the mighty sword", sword, "gives you strength.",
+    "You know you must prevail. For the sake of your village.")
+
+    print("\nYou come to a stop.")
+    print("\nA grisly sight bars your way: a pair of bodies, secured to poles by",
+    "long ropey vines.")
+    print("Wicked vines have wormed their way inside the bodies’", "eyes,"
+    "ears, and mouths.")
+    print("\nTo your horror, you realize the bodies are still moving...")
+    print("\nDo you attack the twitching corpses, or flee in terror?")
+
+    choice = input('> ')
+
+    if "attack" in choice.lower() or "fight" in choice.lower():
+        print("You fight!")
+    elif "flee" in choice.lower() or "run" in choice.lower():
+        print("You run!")
+    else:
+        print("\nYou attempt to",choice.lower(), "but the enemy is upon you first!")
+        global player_hp
+        player_hp -= 1
+        print("\nDead limbs and thorns cut and wound you!")
+
 # Start of the game
 def start():
     print('\nYou stand before a ruined keep which squats atop a low craggy hill.')
@@ -52,17 +85,20 @@ def start():
     'decomposing from within.\n')
 
     print('A sight gives you pause: a ragged banner, depciting a crimson skull',
-    'on a black field, stands high atop the ruined walls.\nWhatever horror',
-    'lurks within has terrorised you and your village for far too long.\n')
+    'on a black field, stands high atop the ruined walls.')
 
     print('You take a deep breath and ready your weapons. The time for',
-    'retribution has come. Do you walk the road up to the keep?')
+    'retribution has come.')
+    print('Whatever horror lurks within has terrorised you and your village',
+    'for far too long.')
+    print("\nAn old dirt road, now overrun with weeds and sickly vines, rises",
+    "towards the ruined citadel.")
+    print('\nDo you walk the road up to the keep?')
 
     while True:
         choice = input('> ')
 
-        print(choice)
-        if choice == "y" or choice == "yes" or choice == "Yes" or choice == "Y":
+        if choice.lower() == "y" or choice.lower() == "yes":
             road_to_keep()
             ###Go to road road()
 
