8d24bbe0a3a2601419ba6f91d388046d81a5bc58
Ben May <benjgmay@googlemail.com>
2018-11-16 22:22:25 +0000

    More tests working

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -1,24 +1,33 @@
 
-lexicon = [('verb', 'go'), ('direction', 'north'), ('direction', 'west')]
+lexicon = [('direction', 'north'), ('direction', 'east'),
+            ('direction', 'west'), ('direction', 'south'),
+            ('noun', 'bear'),
+            ('stop', 'the'), ('stop', 'in'), ('stop', 'of'),
+            ('verb', 'go'), ('verb', 'kill'), ('verb', 'eat')
+            ]
 
 
 def scan(input):
     global lexicon
 #    Scan takes an input and turns it into a list of words
     words = input.split()
+#    print(words)
     sentence = []
 #    Scan searches in lexicon for each of these words
     for word in words:
+#        print("Debug word is >>>>", word)
         for pair in lexicon:
             if word in pair:
 #                print(">>> Word is in", pair)
 #               append tuple pair to sentence
                 sentence.append(pair)
-#                print(">>> Sentence is", sentence)
+
+#    print(">>> Sentence is", sentence)
 
     return sentence
 
 
-scan('north')
+# scan('north')
+# scan('north south east')
 # scan(first_word)
 # scan(' north west')
